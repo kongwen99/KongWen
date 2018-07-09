b2d4f6df6ae46c1bc3e8e4f780e477c922418b4b
kongwen99 <kongwen19920814@163.com>
2018-07-09 16:08:20 +0800

    bean

---
+++ app/src/main/java/top/kongwen/bean/BaseBean.java
@@ -0,0 +1,26 @@
+package top.kongwen.bean;
+
+/**
+ * Created by PEO on 2017/3/11.
+ */
+
+public class BaseBean {
+    private String code;
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
